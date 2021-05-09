26596ad1a226feb6f05ca99fef9476cd17e82fc1
Archiruz <evazerblyre@gmail.com>
2021-05-09 14:26:26 +0700

    fix: GUI & inheritance

--- TiketPesawat PBO/ProjectPBO/src/main/java/com/mycompany/projectpbo/GUI.java
+++ TiketPesawat PBO/ProjectPBO/src/main/java/com/mycompany/projectpbo/GUI.java
@@ -5,25 +5,31 @@
  */
 package com.mycompany.projectpbo;
 
-import javax.swing.JCheckBox;
-
 /**
  *
  * @author USER
  */
-public class GUI extends javax.swing.JFrame implements Tiket, KeretaFunction{
+public class GUI extends javax.swing.JFrame{
 
     /**
      * Creates new form GUI
      */
     public GUI() {
         initComponents();
-
+        alterComponents();
+    }
+    
+    public void alterComponents(){
         Brawijaya kereta1 = new Brawijaya();
         Gajayana kereta2 = new Gajayana();
         Jayabaya kereta3 = new Jayabaya();
         Matarmaja kereta4 = new Matarmaja();
-
+        
+        kereta1.setNama("Brawijaya");
+        kereta2.setNama("Gajayana");
+        kereta3.setNama("Jayabaya");
+        kereta4.setNama("Matarmaja");
+        
         jCheckBox1.setText(kereta1.getNama());
         jCheckBox2.setText(kereta2.getNama());
         jCheckBox3.setText(kereta3.getNama());
@@ -34,8 +40,30 @@ public class GUI extends javax.swing.JFrame implements Tiket, KeretaFunction{
         private String nama;
         private int harga, jumlah;
         
+        //getter
         public String getNama(){
             return nama;
+        }
+        
+        public int getHarga(){
+            return harga;
+        }
+        
+        public int getJumlah(){
+            return jumlah;
+        }
+        
+        //setter
+        public void setNama(String nama){
+            this.nama = nama;
+        }
+        
+        public void setHarga(int harga){
+            this.harga = harga;
+        }
+        
+        public void setJumlah(int jumlah){
+            this.jumlah = jumlah;
         }
     }
     
@@ -78,14 +106,10 @@ public class GUI extends javax.swing.JFrame implements Tiket, KeretaFunction{
         jmlTiket2 = new javax.swing.JTextField();
         jmlTiket3 = new javax.swing.JTextField();
         jmlTiket4 = new javax.swing.JTextField();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
-        jLabel9 = new javax.swing.JLabel();
-        jLabel10 = new javax.swing.JLabel();
-        jLabel11 = new javax.swing.JLabel();
-        jLabel12 = new javax.swing.JLabel();
-        jLabel13 = new javax.swing.JLabel();
-        jLabel14 = new javax.swing.JLabel();
+        jLabel16 = new javax.swing.JLabel();
+        jLabel17 = new javax.swing.JLabel();
+        jLabel18 = new javax.swing.JLabel();
+        jLabel19 = new javax.swing.JLabel();
         jPanel3 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         btnTunai = new javax.swing.JRadioButton();
@@ -130,8 +154,18 @@ public class GUI extends javax.swing.JFrame implements Tiket, KeretaFunction{
         });
 
         jCheckBox3.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        jCheckBox3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBox3ActionPerformed(evt);
+            }
+        });
 
         jCheckBox4.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        jCheckBox4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBox4ActionPerformed(evt);
+            }
+        });
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel4.setText("TIKET");
@@ -168,21 +202,13 @@ public class GUI extends javax.swing.JFrame implements Tiket, KeretaFunction{
             }
         });
 
-        jLabel5.setText("Rp.");
+        jLabel16.setText("1 Tiket = Rp250.000");
 
-        jLabel8.setText("Rp.");
+        jLabel17.setText("1 Tiket = Rp320.000");
 
-        jLabel9.setText("Rp.");
+        jLabel18.setText("1 Tiket = Rp400.000");
 
-        jLabel10.setText("Rp.");
-
-        jLabel11.setText("0");
-
-        jLabel12.setText("0");
-
-        jLabel13.setText("0");
-
-        jLabel14.setText("0");
+        jLabel19.setText("1 Tiket = Rp500.000");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -204,23 +230,17 @@ public class GUI extends javax.swing.JFrame implements Tiket, KeretaFunction{
                             .addComponent(jmlTiket2, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jmlTiket3, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jmlTiket4, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(18, 18, 18)
+                        .addGap(53, 53, 53)
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel8)
-                            .addComponent(jLabel5)
-                            .addComponent(jLabel9)
-                            .addComponent(jLabel10))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel12, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel11, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel13, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel14, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(jLabel19)
+                            .addComponent(jLabel18)
+                            .addComponent(jLabel17)
+                            .addComponent(jLabel16)))
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addGap(174, 174, 174)
                         .addComponent(jLabel4)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(58, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -233,27 +253,22 @@ public class GUI extends javax.swing.JFrame implements Tiket, KeretaFunction{
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jCheckBox1)
                     .addComponent(jmlTiket1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel5)
-                    .addComponent(jLabel11))
+                    .addComponent(jLabel16))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jCheckBox2)
                     .addComponent(jmlTiket2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel8)
-                    .addComponent(jLabel12))
+                    .addComponent(jLabel17))
                 .addGap(18, 18, 18)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel9)
-                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jCheckBox3)
-                        .addComponent(jmlTiket3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jLabel13)))
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jCheckBox3)
+                    .addComponent(jmlTiket3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel18))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jCheckBox4)
                     .addComponent(jmlTiket4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel10)
-                    .addComponent(jLabel14))
+                    .addComponent(jLabel19))
                 .addContainerGap(18, Short.MAX_VALUE))
         );
 
@@ -436,7 +451,7 @@ public class GUI extends javax.swing.JFrame implements Tiket, KeretaFunction{
                     .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGap(0, 12, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -458,47 +473,7 @@ public class GUI extends javax.swing.JFrame implements Tiket, KeretaFunction{
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
-        if(jCheckBox1.isSelected()){
-            jmlTiket1.setEditable(true);
-            jmlTiket1.setText("1");
-        }else{
-            jmlTiket1.setEditable(false);
-            jmlTiket1.setText("0");
-        }
-    }//GEN-LAST:event_jCheckBox1ActionPerformed
-
-    private void jCheckBox2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox2ActionPerformed
-        if(jCheckBox2.isSelected()){
-            jmlTiket2.setEditable(true);
-            jmlTiket2.setText("1");
-        }else{
-            jmlTiket2.setEditable(false);
-            jmlTiket2.setText("0");
-        }
-    }//GEN-LAST:event_jCheckBox2ActionPerformed
-    
-    private void jCheckBox3ActionPerformed(java.awt.event.ActionEvent evt) {                                           
-        if(jCheckBox3.isSelected()){
-            jmlTiket3.setEditable(true);
-            jmlTiket3.setText("1");
-        }else{
-            jmlTiket3.setEditable(false);
-            jmlTiket3.setText("0");
-        }
-    }                                          
-
-    private void jCheckBox4ActionPerformed(java.awt.event.ActionEvent evt) {                                           
-        if(jCheckBox4.isSelected()){
-            jmlTiket4.setEditable(true);
-            jmlTiket4.setText("1");
-        }else{
-            jmlTiket4.setEditable(false);
-            jmlTiket4.setText("0");
-        }
-    }                                          
-
+                                         
     private void jmlTiket1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmlTiket1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jmlTiket1ActionPerformed
@@ -542,6 +517,50 @@ public class GUI extends javax.swing.JFrame implements Tiket, KeretaFunction{
     private void btnTunaiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTunaiActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_btnTunaiActionPerformed
+
+    private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
+        // TODO add your handling code here:
+        if(jCheckBox1.isSelected()){
+            jmlTiket1.setEditable(true);
+            jmlTiket1.setText("1");
+        }else{
+            jmlTiket1.setEditable(false);
+            jmlTiket1.setText("0");
+        }
+    }//GEN-LAST:event_jCheckBox1ActionPerformed
+
+    private void jCheckBox2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox2ActionPerformed
+        // TODO add your handling code here:
+        if(jCheckBox2.isSelected()){
+            jmlTiket2.setEditable(true);
+            jmlTiket2.setText("1");
+        }else{
+            jmlTiket2.setEditable(false);
+            jmlTiket2.setText("0");
+        }
+    }//GEN-LAST:event_jCheckBox2ActionPerformed
+
+    private void jCheckBox3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox3ActionPerformed
+        // TODO add your handling code here:
+        if(jCheckBox3.isSelected()){
+            jmlTiket3.setEditable(true);
+            jmlTiket3.setText("1");
+        }else{
+            jmlTiket3.setEditable(false);
+            jmlTiket3.setText("0");
+        }
+    }//GEN-LAST:event_jCheckBox3ActionPerformed
+
+    private void jCheckBox4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox4ActionPerformed
+        // TODO add your handling code here:
+        if(jCheckBox4.isSelected()){
+            jmlTiket4.setEditable(true);
+            jmlTiket4.setText("1");
+        }else{
+            jmlTiket4.setEditable(false);
+            jmlTiket4.setText("0");
+        }
+    }//GEN-LAST:event_jCheckBox4ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -592,20 +611,16 @@ public class GUI extends javax.swing.JFrame implements Tiket, KeretaFunction{
     private javax.swing.JCheckBox jCheckBox3;
     private javax.swing.JCheckBox jCheckBox4;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel10;
-    private javax.swing.JLabel jLabel11;
-    private javax.swing.JLabel jLabel12;
-    private javax.swing.JLabel jLabel13;
-    private javax.swing.JLabel jLabel14;
     private javax.swing.JLabel jLabel15;
+    private javax.swing.JLabel jLabel16;
+    private javax.swing.JLabel jLabel17;
+    private javax.swing.JLabel jLabel18;
+    private javax.swing.JLabel jLabel19;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
